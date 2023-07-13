880e4d575db53b90b9a72aaffc79d217c5ddef7c
Matt <5d3si7ps@inboxe.anonaddy.com>
2023-07-13 11:31:18 +1000

    Test

---
+++ Example.cs
@@ -0,0 +1,14 @@
+namespace example {
+    class Program {
+    static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            // Here is a comment
+
+            /*
+            Here are some more comments!
+            Love it!
+            */
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HIT339_S223
-Repository for code &amp; assignment files for HIT339, Semester 2 2023.
+Repository for assignment 2, for HIT339 Semester 2 2023.
+
+# Contributors
+
+- Kyle Esson
+- Matthew Bonfanti
+- 
