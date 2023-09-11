c684264e1c6550acc9aa0a1011c56eadec119bd6
study <S349051@students.cdu.edu.au>
2023-09-11 10:42:47 +1000

    The models might work…?

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,46 +1,58 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace e_corp.Models
 {
-	public class Coach
-	{
-        public int Id { get; set; }
-        [Required]
-        [StringLength(500)]
-        public string Biography { get; set; }
-        [Required]
-        public IdentityUser User { get; set; }
-    }
-
     public class Session
     {
-        public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
+        // Keys
+        public int SessionID { get; set; }
         public string Name { get; set; }
-        [Required]
         public DateTime Date { get; set; }
-        public IdentityUser User { get; set; }
+
+        // Foreign Keys
+        public string CoachID { get; set; }
+        public int LocationID { get; set; }
+
+        // Navigation Properties
+        [ForeignKey("CoachID")]
+        public IdentityUser Coach { get; set; }
+        [ForeignKey("LocationID")]
         public Location Location { get; set; }
     }
 
     public class Booking
     {
-        public int Id { get; set; }
-        [Required]
+        // Keys
+        public int BookingID { get; set; }
+
+        // Foreign Keys
+        public int SessionID { get; set; }
+        public int UserID { get; set; }
+
+        // Navigation Properties
+        [ForeignKey("SessionID")]
         public Session Session { get; set; }
-        [Required]
-        public IdentityUser User { get; set; }
+        [ForeignKey("UserID")]
+        public IdentityUser Member { get; set; }
     }
 
     public class Location
     {
-        public int Id { get; set; }
-        [Required]
+        // Keys
+        public int LocationID { get; set; }
         public string Name { get; set; }
-        [Required]
         public string Address { get; set; }
     }
-}
 
+    public class Biography
+    {
+        // Keys
+        public int CoachID { get; set; }
+        public string Content { get; set; }
+
+        // Foreign Keys
+        public int UserID { get; set; }
+    }
+}
